99c3aac119ab462d53620e0882e8171131e0c92a
Hector Simpson <hector@me.com>
2017-05-09 14:20:23 +0100

    Add tests

--- test/test.js
+++ test/test.js
@@ -5,17 +5,73 @@ chai.use(chaiHttp);
 
 const server = require('../index');
 
-describe('server', () => {
-  it('should respond with 200', (done) => {
+describe('when the page loads', () => {
+  it('it should respond with a 200', (done) => {
     chai.request(server)
     .get('/')
     .end( (err, res) => {
       // there should be no errors
       should.not.exist(err);
-      
+
       // there should be a 200 status code
       res.status.should.equal(200);
-      
+
+      done();
+    });
+  });
+
+  it('it should display the correct content', (done) => {
+    chai.request(server)
+    .get('/')
+    .end( (err, res) => {
+      // there should be no errors
+      should.not.exist(err);
+
+      // there should be a 200 status code
+      res.status.should.equal(200);
+
+      done();
+    });
+  });
+
+  it('it should display the correct environment', (done) => {
+    chai.request(server)
+    .get('/')
+    .end( (err, res) => {
+      // there should be no errors
+      should.not.exist(err);
+
+      // there should be a 200 status code
+      res.status.should.equal(200);
+
+      done();
+    });
+  });
+
+  it('it should not show a 404', (done) => {
+    chai.request(server)
+    .get('/')
+    .end( (err, res) => {
+      // there should be no errors
+      should.not.exist(err);
+
+      // there should be a 200 status code
+      res.status.should.equal(200);
+
+      done();
+    });
+  });
+
+  it('it should load correctly', (done) => {
+    chai.request(server)
+    .get('/')
+    .end( (err, res) => {
+      // there should be no errors
+      should.not.exist(err);
+
+      // there should be a 200 status code
+      res.status.should.equal(200);
+
       done();
     });
   });
