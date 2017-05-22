217bbde0a5c0153536c8ab922672454a57738846
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2017-05-22 10:08:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("App is running at localhost:" + app.get('port'));
 });
 
 module.exports = app
